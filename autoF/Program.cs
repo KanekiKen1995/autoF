b340f78c1d96468628771c52a459096c9672f8d7
lamvu <lam.vu@ntq-soluiton.com.vn>
2021-02-17 09:31:58 +0700

    first commit

---
+++ autoF/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace autoF
+{
+    class Program
+    {
+        static IWebDriver driver;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("auto F");
+            Post();
+        }
+
+
+        static void Post()
+        {
+            NewWindown("https://www.facebook.com/");
+
+        }
+        static void NewWindown(string url)
+        {
+           var pathChormeDỉver = Path.GetDirectoryName(Path.GetFullPath(@"chromedriver.exe"));
+            driver = new ChromeDriver(pathChormeDỉver);
+            driver.Url = url;
+            //driver.Close();
+        }
+
+    }
+}
